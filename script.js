8ed3bcc6db0d637f2f2bffbb0ed73b8633e7addc
ayoubayoubbengara <44469769+ayoubayoubbengara@users.noreply.github.com>
2020-04-10 01:19:16 +0100

    clock with JS
    dirst thing i add a simple JS code
    and second i chang some colors for the clock design it looks better than the first one

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 // Séléctionner les aiguilles de montre
+const deg = 6;
 const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
-
-
-//Ajouter l'heure , minite , seconde  dans des varaiables
-
-
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-
-
-// Déplacer les aiguilles 
-function demarrerLaMontre() {
-
-
-
-}
-// Exercuter la fonction chaque second
-var interval = setInterval(demarrerLaMontre, 1000);
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    AIGUILLEHR.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    AIGUILLEMIN.style.transform = `rotateZ(${mm}deg)`;
+    AIGUILLESEC.style.transform = `rotateZ(${ss}deg)`;
+})
